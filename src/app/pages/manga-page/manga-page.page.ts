97eef29b2c99e63751f93a4a3a005a6618d98f4a
Adrian <adriker01@gmail.com>
2022-05-22 18:07:45 +0200

    Commit

--- src/app/pages/manga-page/manga-page.page.ts
+++ src/app/pages/manga-page/manga-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-manga-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MangaPagePage implements OnInit {
 
-  constructor() { }
+  constructor(private menuController:MenuController) { }
 
   ngOnInit() {
+  }
+
+  mostrarMenu(){
+    this.menuController.open('first');
   }
 
 }
