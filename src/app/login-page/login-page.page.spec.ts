5c5cd2455c2fcb3ff539e0712e9afd901bcea0d1
Andrés <01andres2012@gmail.com>
2022-06-05 17:45:34 +0200

    commit

--- src/app/login-page/login-page.page.spec.ts
+++ src/app/login-page/login-page.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { LoginPagePage } from './login-page.page';
+import { LoginPage } from './login-page.page';
 
 describe('LoginPagePage', () => {
-  let component: LoginPagePage;
-  let fixture: ComponentFixture<LoginPagePage>;
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginPagePage ],
+      declarations: [ LoginPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(LoginPagePage);
+    fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
