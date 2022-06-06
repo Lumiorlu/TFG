8427c4bb65514ce20899b1dac8b6f77f290fa10d
Adrian <adriker01@gmail.com>
2022-06-06 17:35:04 +0200

    Commit

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat';
+import 'firebase/compat/storage';
+import { environment } from '../../environments/environment.prod';
+
+firebase.initializeApp(environment.firebase)
 
 @Injectable({
   providedIn: 'root'
