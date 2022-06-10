83bb81960703ff0cd6fde2252f08a823d06f7170
Andrés <01andres2012@gmail.com>
2022-06-10 13:47:27 +0200

    falta borrar cuenta

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: 'TFG',
   webDir: 'www',
-  bundledWebRuntime: false,
-  cordova: {
-    preferences: {
-      ScrollEnabled: 'false',
-      BackupWebStorage: 'none',
-      SplashMaintainAspectRatio: 'true',
-      FadeSplashScreenDuration: '300',
-      SplashShowOnlyFirstTime: 'false',
-      SplashScreen: 'screen',
-      SplashScreenDelay: '3000'
-    }
-  }
+  bundledWebRuntime: false
 };
 
 export default config;
